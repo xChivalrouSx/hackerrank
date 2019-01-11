903f1c335c8bfbe3e2d454816b71f89bc60dc68b
Mert Çakar <mertcakar.es@gmail.com>
2019-01-11 03:39:23 +0300

    Solve Me First - Empty

---
+++ hackerrank/Classes/ProblemSolving.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerrank.Classes
+{
+    static class ProblemSolving
+    {
+
+        #region [ - Solve Me First - ]
+
+        // Problem Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+
+        public static void solveMeFirst_Main()
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+
+        private static int solveMeFirst(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            
+        }
+
+        #endregion
+
+    }
+}
---
+++ hackerrank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hackerrank.Classes;
+
+namespace hackerrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemSolving.solveMeFirst_Main();
+
+
+
+        }
+    }
+}
