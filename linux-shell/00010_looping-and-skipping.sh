5c2e63f9f59eb929aa264c3d5532b2076ee23a15
Mert Çakar <mertcakar.es@gmail.com>
2024-02-07 23:59:26 +0300

    Answer added for 'Looping and Skipping'.

---
+++ linux-shell/00010_looping-and-skipping.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Looping and Skipping
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-and-skipping/problem?isFullScreen=true
+
+for((i=1; i<100; i=i+2))
+do
+   echo $i
+done  
