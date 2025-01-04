f4f341e767452fca70730eae680da72c39810d28
Mert Çakar <mertcakar.es@gmail.com>
2025-01-05 00:24:36 +0300

    Answer added for challenge called 'Code Review Feedback'.

---
+++ react/src/app/0008/components/CodeReviewFeedback.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+
+const FeedbackSystem = () => {
+
+  const [values, setValues] = useState([
+    {
+      "name" : "Readability",
+      "up" : 0,
+      "down" : 0      
+    },
+    {
+      "name" : "Performance",
+      "up" : 0,
+      "down" : 0      
+    },
+    {
+      "name" : "Security",
+      "up" : 0,
+      "down" : 0      
+    },
+    {
+      "name" : "Documentation",
+      "up" : 0,
+      "down" : 0      
+    },
+    {
+      "name" : "Testing",
+      "up" : 0,
+      "down" : 0      
+    },
+  ]);
+
+  const setSpecificValue = (name, isUp) => {
+    setValues(values.map(value => {
+      if (value.name === name) {
+        isUp ? value.up++ : value.down++;
+      }
+      return value;
+    }));
+  };
+
+  return (
+      <div className="my-0 mx-auto text-center w-mx-1200">
+        <div className="flex wrap justify-content-center mt-30 gap-30">
+          {
+            values.map((value, index) => 
+                <div className="pa-10 w-300 card" key={"base-div" + index}>
+                    <h2>{value.name}</h2>
+                    <div className="flex my-30 mx-0 justify-content-around">
+                        <button className="py-10 px-15" data-testid={"upvote-btn-" + index} onClick={() => setSpecificValue(value.name, true)}>
+                            👍 Upvote
+                        </button>
+                        <button className="py-10 px-15 danger" data-testid={"downvote-btn-" + index} onClick={() => setSpecificValue(value.name, false)}>
+                            👎 Downvote
+                        </button>
+                    </div>
+                    <p className="my-10 mx-0" data-testid={"upvote-count-" + index}>
+                        Upvotes: <strong>{value.up}</strong>
+                    </p>
+                    <p className="my-10 mx-0" data-testid={"downvote-count-" + index}>
+                        Downvotes: <strong>{value.down}</strong>
+                    </p>
+                </div>
+            )
+          }
+        </div>
+      </div>
+    )};
+
+export default FeedbackSystem;
---
+++ react/src/app/0008/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+import FeedbackSystem from "./components/CodeReviewFeedback";
+
+
+const App = () => {
+  return <FeedbackSystem />;
+};
+
+export default App;
