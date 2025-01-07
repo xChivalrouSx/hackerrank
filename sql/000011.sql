41bdd722cb60bf02ccec0b2710c357feafcbc383
Mert Çakar <mertcakar.es@gmail.com>
2025-01-08 01:33:31 +0300

    Answer added for challenge called 'Weather Observation Station '.

---
+++ sql/000011.sql
@@ -0,0 +1,5 @@
+-- Weather Observation Station 4
+-- https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+
+SELECT  COUNT(CITY) - COUNT(DISTINCT(CITY))
+FROM    STATION;
