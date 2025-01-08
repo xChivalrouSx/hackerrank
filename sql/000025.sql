6552a9cf9faa3422932fbf2a43302e324ad59c18
Mert Çakar <mertcakar.es@gmail.com>
2025-01-09 00:01:23 +0300

    Answer added for challenge called 'Revising Aggregations - Averages'.

---
+++ sql/000025.sql
@@ -0,0 +1,6 @@
+-- Revising Aggregations - Averages
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+
+SELECT  AVG(POPULATION)
+FROM    CITY
+WHERE   DISTRICT = 'California';
