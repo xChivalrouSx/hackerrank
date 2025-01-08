9140885a84b1d1483c50b1f7fcb04a8b24635dc3
Mert Çakar <mertcakar.es@gmail.com>
2025-01-09 00:29:32 +0300

    Answer added for challenge called 'Population Density Difference'.

---
+++ sql/000028.sql
@@ -0,0 +1,5 @@
+-- Population Density Difference
+-- https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+
+SELECT  MAX(POPULATION) - MIN(POPULATION)
+FROM    CITY;
