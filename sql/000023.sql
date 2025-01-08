3dd981b3f691a5e4ded0b201ed83aba88b418b52
Mert Çakar <mertcakar.es@gmail.com>
2025-01-08 23:54:48 +0300

    Answer added for challenge called 'Revising Aggregations - The Count Function'.

---
+++ sql/000023.sql
@@ -0,0 +1,6 @@
+-- Revising Aggregations - The Count Function
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+
+SELECT  COUNT(*)
+FROM    CITY
+WHERE   POPULATION > 100000;
