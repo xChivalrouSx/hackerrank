57435b2ad9741d4b80ed25c28e1c95bd4d33479e
Mert Çakar <mertcakar.es@gmail.com>
2025-01-08 23:58:03 +0300

    Answer added for challenge called 'Revising Aggregations - The Sum Function'.

---
+++ sql/000024.sql
@@ -0,0 +1,6 @@
+-- Revising Aggregations - The Sum Function
+-- https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+
+SELECT  SUM(POPULATION)
+FROM    CITY
+WHERE   DISTRICT = 'California';
