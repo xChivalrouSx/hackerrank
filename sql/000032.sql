ef921e58dc022f98416c3a382485a94535b8475e
Mert Çakar <mertcakar.es@gmail.com>
2025-01-09 22:03:28 +0300

    Answer added for challenge called 'African Cities'.

---
+++ sql/000032.sql
@@ -0,0 +1,7 @@
+-- African Cities
+-- https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true
+
+SELECT  c1.NAME
+FROM    CITY c1
+        LEFT JOIN COUNTRY c2 ON (c1.COUNTRyCODE = c2.CODE)
+WHERE   c2.CONTINENT = 'Africa';
