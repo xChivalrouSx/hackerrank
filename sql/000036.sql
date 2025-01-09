b6b61a872ba3cc68a332ed697c24596126eb1f0b
Mert Çakar <mertcakar.es@gmail.com>
2025-01-09 23:48:03 +0300

    Answer added for challenge called 'Weather Observation Station 13'.

---
+++ sql/000036.sql
@@ -0,0 +1,8 @@
+-- Weather Observation Station 13
+-- https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
+
+SELECT  ROUND(SUM(LAT_N), 4)
+FROM    STATION
+WHERE   LAT_N > 38.7880
+        AND
+        LAT_N < 137.2345;
