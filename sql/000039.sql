17fd364166a3330bbeea69ecadfd6e78b11ef6b4
Mert Çakar <mertcakar.es@gmail.com>
2025-01-10 00:07:04 +0300

    Answer added for challenge called 'Weather Observation Station 16'.

---
+++ sql/000039.sql
@@ -0,0 +1,6 @@
+-- Weather Observation Station 16
+-- https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true
+
+SELECT  ROUND(MIN(LAT_N), 4)
+FROM    STATION
+WHERE   LAT_N > 38.7760;
