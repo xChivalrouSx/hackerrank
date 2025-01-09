2f40cb9cd6944c54e85afd997a6c80ba1e9084a4
Mert Çakar <mertcakar.es@gmail.com>
2025-01-09 22:23:21 +0300

    Answer added for challenge called 'Average Population of Each Continent' and fix some typo.

---
+++ sql/000033.sql
@@ -0,0 +1,7 @@
+-- Average Population of Each Continent
+-- https://www.hackerrank.com/challenges/average-population-of-each-continent?isFullScreen=true
+
+SELECT      c2.CONTINENT, FLOOR(AVG(c1.POPULATION))
+FROM        CITY c1
+            JOIN COUNTRY c2 ON (c1.COUNTRYCODE = c2.CODE)
+GROUP BY    c2.CONTINENT;
