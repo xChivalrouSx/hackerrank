00e7e103674a32b39ce31fd448e5e429cb3adcff
Mert Çakar <mertcakar.es@gmail.com>
2025-01-11 00:58:17 +0300

    Answer added for challenge called 'Binary Tree Nodes'.

---
+++ sql/000044.sql
@@ -0,0 +1,11 @@
+-- Binary Tree Nodes
+-- https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
+SELECT      N,
+            CASE
+                WHEN P IS NULL THEN 'Root'
+                WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+                ELSE 'Leaf'
+            END
+FROM        BST
+ORDER BY    N;
