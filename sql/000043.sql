395a1988efcd4a295982b066f03fb1dbd1bb3c65
Mert Çakar <mertcakar.es@gmail.com>
2025-01-11 00:38:11 +0300

    Answer added for challenge called 'Weather Observation Station 19'.

---
+++ sql/000043.sql
@@ -0,0 +1,7 @@
+-- Weather Observation Station 19
+-- https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+
+SELECT  ROUND(SQRT(
+                POWER(MAX(LAT_N) - MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+            ), 4)
+FROM    STATION;
