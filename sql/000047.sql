89f45d1061aa3770bf6c269b165b8b94f7a6a5e2
Mert Çakar <mertcakar.es@gmail.com>
2025-01-13 00:10:23 +0300

    Answer added for challenge called 'Weather Observation Station 20'.

---
+++ sql/000047.sql
@@ -0,0 +1,5 @@
+-- Weather Observation Station 20
+-- https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
+
+SELECT  ROUND(MEDIAN(LAT_N), 4)
+FROM    STATION;
