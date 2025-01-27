317eeb71ee84a83f0aa6709bc87786f366739c2c
Mert Çakar <mertcakar.es@gmail.com>
2025-01-27 23:57:27 +0300

    Answer is added for challenge called 'Concatenate an array with itself'.

---
+++ linux-shell/00041_HackerrankLinuxShell.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Concatenate an array with itself
+# https://www.hackerrank.com/challenges/bash-tutorials-concatenate-an-array-with-itself/problem?isFullScreen=true
+
+array=$(xargs)
+echo $array $array $array
