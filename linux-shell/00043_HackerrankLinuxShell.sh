629f0a3d171e21d1e7af89b6837d44414ff8df1d
Mert Çakar <mertcakar.es@gmail.com>
2025-01-30 23:17:34 +0300

    Missing code is added for challenge called 'Count the number of elements in an Array'.

--- linux-shell/00043_HackerrankLinuxShell.sh
+++ linux-shell/00043_HackerrankLinuxShell.sh
@@ -3,3 +3,5 @@
 # Count the number of elements in an Array
 # https://www.hackerrank.com/challenges/bash-tutorials-count-the-number-of-elements-in-an-array/problem?isFullScreen=true
 
+readarray array
+echo ${#array[@]}
