5fcef58d812ab0e486eeb1f3931281153e2cc92b
Mert Çakar <mertcakar.es@gmail.com>
2025-02-08 23:52:16 +0300

    gitignore is updated.

--- linux-shell/00055_HackerrankLinuxShell.sh
+++ linux-shell/00055_HackerrankLinuxShell.sh
@@ -3,7 +3,6 @@
 # Remove the First Capital Letter from Each Element
 # https://www.hackerrank.com/challenges/bash-tutorials-remove-the-first-capital-letter-from-each-array-element/problem?isFullScreen=true
 
-
 while read -r line;
 do
   echo -n ".${line:1} "
--- linux-shell/00056_HackerrankLinuxShell.sh
+++ linux-shell/00056_HackerrankLinuxShell.sh
@@ -3,5 +3,4 @@
 # 'Sed' command #1
 # https://www.hackerrank.com/challenges/text-processing-in-linux-the-sed-command-1/problem?isFullScreen=true
 
-
 sed 's/the /this /'
