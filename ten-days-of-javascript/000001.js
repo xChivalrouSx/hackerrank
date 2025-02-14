7796b99d7b2a17b458d193ac256cd43c85f3cfa7
Mert Çakar <mertcakar.es@gmail.com>
2025-02-14 21:03:53 +0300

    Answer is added for challenge called 'Day 0: Hello, World!'.

---
+++ ten-days-of-javascript/000001.js
@@ -0,0 +1,7 @@
+// Day 0: Hello, World!
+// https://www.hackerrank.com/challenges/js10-hello-world/problem?isFullScreen=true
+
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+}
