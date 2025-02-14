4f4fc7beea298d96a8abe154b38f2dfc77aef63a
Mert Çakar <mertcakar.es@gmail.com>
2025-02-14 21:10:21 +0300

    Answer is added for challenge called 'Day 0: Data Types'.

---
+++ ten-days-of-javascript/000002.js
@@ -0,0 +1,12 @@
+// Day 0: Data Types
+// https://www.hackerrank.com/challenges/js10-data-types/problem?isFullScreen=true
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    
+    console.log(firstInteger + parseInt(secondInteger))  
+    console.log(firstDecimal + parseFloat(secondDecimal))
+    console.log(firstString + secondString)
+}
