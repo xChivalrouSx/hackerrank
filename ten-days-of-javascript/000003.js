79c12a2f6b5cfb278455c6cef9967c1bf6b23504
Mert Çakar <mertcakar.es@gmail.com>
2025-02-15 23:42:18 +0300

    Answer is added for challenge called 'Day 1: Arithmetic Operators'.

---
+++ ten-days-of-javascript/000003.js
@@ -0,0 +1,10 @@
+// Day 1: Arithmetic Operators
+// https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem?isFullScreen=true
+
+function getArea(length, width) {
+    return length * width;
+}
+
+function getPerimeter(length, width) {
+    return (length + width) * 2;
+}
