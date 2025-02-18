a3136c496a63b2d9806b5bfd485c5e07a50a4041
Mert Çakar <mertcakar.es@gmail.com>
2025-02-18 23:33:35 +0300

    Answer is added for challenge called 'Day 4: Create a Rectangle Object'.

---
+++ ten-days-of-javascript/000012.js
@@ -0,0 +1,11 @@
+// Day 4: Create a Rectangle Object
+// https://www.hackerrank.com/challenges/js10-objects/problem?isFullScreen=true
+
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    }
+}
