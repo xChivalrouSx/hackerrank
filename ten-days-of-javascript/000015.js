f7e65d709106f95d18879bf20f9f4a4dcc17a318
Mert Çakar <mertcakar.es@gmail.com>
2025-02-19 23:40:04 +0300

    Answer is added for challenge called 'Day 5: Inheritance'.

---
+++ ten-days-of-javascript/000015.js
@@ -0,0 +1,14 @@
+// Day 5: Inheritance
+// https://www.hackerrank.com/challenges/js10-inheritance/problem?isFullScreen=true
+
+Rectangle.prototype.area = function() {
+    return this.w * this.h;
+}
+
+class Square extends Rectangle {
+
+    constructor(s) {
+        super(s, s);
+    }
+    
+}
