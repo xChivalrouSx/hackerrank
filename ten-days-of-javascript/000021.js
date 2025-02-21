d502b89feb01dba5a6c4c87b9a25fe3893f5e5b0
Mert Çakar <mertcakar.es@gmail.com>
2025-02-22 00:55:53 +0300

    Answer is added for challenge called 'Day 7: Regular Expressions II'.

---
+++ ten-days-of-javascript/000021.js
@@ -0,0 +1,6 @@
+// Day 7: Regular Expressions II
+// https://www.hackerrank.com/challenges/js10-regexp-2/problem?isFullScreen=true
+
+function regexVar() {
+    return new RegExp(/^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/);
+}
