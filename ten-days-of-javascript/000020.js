5ee394deb29543347a0632b0db2a6ce320fc6da3
Mert Çakar <mertcakar.es@gmail.com>
2025-02-22 00:54:19 +0300

    Answer is added for challenge called 'Day 7: Regular Expressions I'.

---
+++ ten-days-of-javascript/000020.js
@@ -0,0 +1,6 @@
+// Day 7: Regular Expressions I
+// https://www.hackerrank.com/challenges/js10-regexp-1/problem?isFullScreen=true
+
+function regexVar() {
+    return new RegExp(/^([aeiou]).+\1$/);
+}
