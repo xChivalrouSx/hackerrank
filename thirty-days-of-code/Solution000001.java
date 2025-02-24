d20b61cd578f4bd628c3e06da3b5f8c7984fd4ec
Mert Çakar <mertcakar.es@gmail.com>
2025-02-24 23:51:14 +0300

    Answer is added for challenge called 'Day 0: Hello, World.'.

---
+++ thirty-days-of-code/Solution000001.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution000001 {
+
+    // Day 0: Hello, World.
+    // https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("Hello, World.");
+        System.out.println(input);
+    }
+}
